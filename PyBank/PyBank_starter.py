b2de4d339fd8be70e20c8f6b7ecc1f61aba202a8
Ricardo Andres Luna <Ricardo.AndresLuna@fele.com>
2024-11-09 13:25:17 -0600

    First commit

--- PyBank/PyBank_starter.py
+++ PyBank/PyBank_starter.py
@@ -6,13 +6,18 @@ import csv
 import os
 
 # Files to load and output (update with correct file paths)
-file_to_load = os.path.join("Resources", "budget_data.csv")  # Input file path
-file_to_output = os.path.join("analysis", "budget_analysis.txt")  # Output file path
+file_to_load = os.path.join("PyBank\Resources", "budget_data.csv")  # Input file path
+file_to_output = os.path.join("PyBank","analysis", "budget_analysis.txt")  # Output file path
 
 # Define variables to track the financial data
 total_months = 0
 total_net = 0
 # Add more variables to track other necessary financial data
+monthly_change = []
+net_change_list = []
+greatest_increase_list = ["",0]
+greatest_decrease_list = ["",99999999999]
+prev_net = 0
 
 # Open and read the csv
 with open(file_to_load) as financial_data:
@@ -22,35 +27,51 @@ with open(file_to_load) as financial_data:
     header = next(reader)
 
     # Extract first row to avoid appending to net_change_list
-
+    first_row = next(reader)
 
     # Track the total and net change
-
+    total_net += int(first_row[1])
+    prev_net += int(first_row[1]) 
 
     # Process each row of data
     for row in reader:
 
         # Track the total
-
+        total_net += int(row[1])
 
         # Track the net change
-
+        total_months += 1
+        net_change = int(row[1]) - prev_net
+        prev_net = int(row[1])
+        net_change_list += [net_change]
+        monthly_change += [row[0]]
 
         # Calculate the greatest increase in profits (month and amount)
-
+        if net_change > greatest_increase_list[1]:
+            greatest_increase_list[0] = row[0]
+            greatest_increase_list[1] = net_change
 
         # Calculate the greatest decrease in losses (month and amount)
-
-
+        if net_change < greatest_decrease_list[1]:
+            greatest_decrease_list[0] = row[0]
+            greatest_decrease_list[1] = net_change
 
 # Calculate the average net change across the months
-
+avg = sum(net_change_list) / len(net_change_list)
 
 # Generate the output summary
-
+output = (
+    f"Financial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${total_net}\n"
+    f"Average Change: ${avg:.2f}\n"
+    f"Greatest Increase in Profits: {greatest_increase_list[0]} (${greatest_increase_list[1]})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease_list[0]} (${greatest_decrease_list[1]})\n"
+)
 
 # Print the output
-
+print(output)
 
 # Write the results to a text file
 with open(file_to_output, "w") as txt_file:
--- PyPoll/PyPoll_starter.py
+++ PyPoll/PyPoll_starter.py
@@ -6,17 +6,19 @@ import csv
 import os
 
 # Files to load and output (update with correct file paths)
-file_to_load = os.path.join("Resources", "election_data.csv")  # Input file path
-file_to_output = os.path.join("analysis", "election_analysis.txt")  # Output file path
+file_to_load = os.path.join("PyPoll","Resources", "election_data.csv")  # Input file path
+file_to_output = os.path.join("PyPoll","analysis", "election_analysis.txt")  # Output file path
 
 # Initialize variables to track the election data
 total_votes = 0  # Track the total number of votes cast
 
 # Define lists and dictionaries to track candidate names and vote counts
-
+vote_count_tracker = {}
+candidate_name_list = []
 
 # Winning Candidate and Winning Count Tracker
-
+winner_candidate = ""
+winner_votes = 0
 
 # Open the CSV file and process it
 with open(file_to_load) as election_data:
@@ -29,42 +31,60 @@ with open(file_to_load) as election_data:
     for row in reader:
 
         # Print a loading indicator (for large datasets)
-        print(". ", end="")
+        #print(". ", end="")
 
         # Increment the total vote count for each row
-
+        total_votes = total_votes + 1
 
         # Get the candidate's name from the row
-
+        candidate_name = row[2]
 
         # If the candidate is not already in the candidate list, add them
-
+        if candidate_name not in candidate_name_list:
+            candidate_name_list.append(candidate_name)
+            vote_count_tracker[candidate_name] = 0
 
         # Add a vote to the candidate's count
-
+        vote_count_tracker[candidate_name] += 1
 
 # Open a text file to save the output
 with open(file_to_output, "w") as txt_file:
 
     # Print the total vote count (to terminal)
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,.0f}\n"
+        f"-------------------------\n")
 
+    print(election_results)
 
     # Write the total vote count to the text file
-
+    txt_file.write(election_results)
 
     # Loop through the candidates to determine vote percentages and identify the winner
-
+    for candidate in vote_count_tracker:
 
         # Get the vote count and calculate the percentage
-
+        vote_count_candidate = vote_count_tracker[candidate]
+        vote_percentage_candidate = (vote_count_candidate / total_votes)*100
 
         # Update the winning candidate if this one has more votes
-
-
+        if vote_count_candidate > winner_votes:
+            winner_votes = vote_count_candidate
+            winner_candidate = candidate
+        
         # Print and save each candidate's vote count and percentage
-
+        candidate_results = f"{candidate}: {vote_percentage_candidate:.3f}% ({vote_count_candidate:,.0f})\n"
+        print(candidate_results)
+        txt_file.write(candidate_results)
 
     # Generate and print the winning candidate summary
-
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winner_candidate}\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
 
     # Save the winning candidate summary to the text file
+    txt_file.write(winning_candidate_summary)
